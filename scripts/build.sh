1a3685b19b02d9dbcd46cd54ea81645f67864be6
Ivanov Dmitry <errfrom@yandex.ru>
2017-10-21 21:30:13 +0300

    Project renamed.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,15 +2,16 @@
 
 function create_app_dir()
 {
-  CABAL_ALL=`cat ../Glob.cabal`
+  CABAL_ALL=`cat ./ConvEm.cabal`
   VERSION_STRING=$(echo "$CABAL_ALL" | grep "^version:.*$")
   VERSION=$(echo "$VERSION_STRING" | sed 's/version: *//')
-  DIR_NAME="Glob-$VERSION-linix-x86_64"
+  DIR_NAME="ConvEm-$VERSION-linix-x86_64"
   mkdir "$DIR_NAME"
 }
 
+cd ..
 stack clean
 create_app_dir
 stack build --executable-stripping --library-stripping
-cp ../.stack-work/install/x86_64-linux/lts-*/*/bin/*-exe "$DIR_NAME/Glob"
+cp ./.stack-work/install/x86_64-linux/lts-*/*/bin/*-exe $DIR_NAME
 stack clean
